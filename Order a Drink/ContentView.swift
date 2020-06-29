9e57fb6515236adaa93f5acf74cea2c0f2add27a
John Klay <jtheklay@gmail.com>
2020-06-29 17:01:40 -0400

    yay

---
+++ Order a Drink/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Order a Drink
+//
+//  Created by Omar on 6/29/20.
+//  Copyright © 2020 Orange Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
